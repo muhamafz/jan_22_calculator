076d43016ea5d4e7f075ef1b0c17149bba1a3e8a
Afzal M <mafzal2005@gmail.com>
2019-01-22 21:10:21 -0500

    using the sum function instead of manual way

--- calculator.py
+++ calculator.py
@@ -3,12 +3,4 @@ a calculator library
 """
 
 def add(*args):
-    answer = 0
-    for value in args:
-        answer += value
-    return answer
-
-def test_negative_value():
-    """
-    negative"""
-    assert add(-1,-1,-1,-1,-1) == -5
+    return sum(args)
